61f194fdfe60d434d8f585e2b351c42ace38283a
Siddharth <mitsiddharth@gmail.com>
2012-12-08 17:33:58 -0600

    Replaced DOMNodeInserted with another alternative.

--- main.js
+++ main.js
@@ -7,6 +7,7 @@
 	var answer_div_selector = ".row .pagedlist_item";
 	var answer_anchor_tag_selector = "a[name*='answer_']";
 	var answer_text_selector = ".answer_text";
+	var answer_content_selector = ".answer_content";
 
 	var nAnswers = 0;	
 
@@ -19,19 +20,25 @@
 
  	var getTargetUrl = function(element) {
     	var currentUrl = $(element).parents(answer_div_selector).find(answer_anchor_tag_selector).attr('name');
+      console.log(ansHashTags[currentUrl] + 1);
+      console.log(currentUrl);
+      console.log(ansHashIndex[ansHashTags[currentUrl] + 1]);
     	return ansHashIndex[ansHashTags[currentUrl] + 1];
 	};
 
   var mouseOverHandler = function(e, left_coord) {
     var top_coord = e.pageY;
     var nextUrl = '#' + getTargetUrl(e.srcElement);
-    var skipButton = createSkipButton(left_coord - 50, top_coord, nextUrl);
-    skipButton.appendTo(document.body);
+    if(nextUrl != '#undefined') {
+      var skipButton = createSkipButton(left_coord - 50, top_coord, nextUrl);
+      skipButton.appendTo(document.body);
+    }
   };
 
  	var activate_mouseover_events = function() {
-    	var answer_elements = $(answer_text_selector);
-    	var left_coord = answer_elements.offset().left;
+      var answer_text = $(answer_text_selector);
+    	var answer_elements = $(answer_content_selector);
+    	var left_coord = answer_text.offset().left;
 
       answer_elements.unbind('mouseover');
 
@@ -78,7 +85,7 @@
                      '    </span>'+
                      '  </a>'+
                      '</div>');
-      skipButton.css({'left':x_coord, 'top':y_coord, 'position':'absolute'});
+      skipButton.css({'left':x_coord, 'top':y_coord, 'position':'absolute', 'margin': '5px'});
     } else {
       skipButton.css({'left':x_coord, 'top':y_coord});
     }
@@ -89,7 +96,6 @@
   var registerDomInsertEvent = function() {
     $(document).bind('DOMNodeInserted', function(e) {
       var element = e.target;
-      console.log(element);
       if($(element).attr('class') == 'pagedlist_item') {
         populateAnswerHash(); 
         activate_mouseover_events();
@@ -98,8 +104,16 @@
   };
 
   var registerEvents = function() {
-    activate_mouseover_events();
-    registerDomInsertEvent();
+    //activate_mouseover_events();
+    //registerDomInsertEvent();
   }
 
+  var animationEvent = function(e) {
+      if(e.originalEvent.animationName == 'nodeInserted') {
+      populateAnswerHash();
+      activate_mouseover_events();
+    }
+  }
+
+  $(document).bind('webkitAnimationStart', function(e) {animationEvent(e)});
 })(jQuery);
